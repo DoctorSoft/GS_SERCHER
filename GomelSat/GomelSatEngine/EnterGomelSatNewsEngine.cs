4b01ce873ca3eff4f858e97e4e8671b9580264d3
ArcherFromGrodno <ArcherFromGrodno@gmail.com>
2016-08-06 01:44:47 +0300

    [Develop] Some small changes were made

--- GomelSat/GomelSatEngine/EnterGomelSatNewsEngine.cs
+++ GomelSat/GomelSatEngine/EnterGomelSatNewsEngine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
